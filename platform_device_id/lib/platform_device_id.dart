9f48d02c365b724cae649fc914b5130387acd9b8
Vitaly Zeenko <dev.zeenko@gmail.com>
2025-03-13 17:21:28 +0100

    Update dependencies and project configuration for Android and Flutter

--- platform_device_id/lib/platform_device_id.dart
+++ platform_device_id/lib/platform_device_id.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 import 'dart:io';
 import 'package:flutter/services.dart';
-import 'package:device_info/device_info.dart';
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:platform_device_id_platform_interface/platform_device_id_platform_interface.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
 
@@ -18,7 +18,7 @@ class PlatformDeviceId {
         deviceId = await PlatformDeviceIdPlatform.instance.getDeviceId();
       } else if (Platform.isAndroid) {
         AndroidDeviceInfo androidInfo = await deviceInfoPlugin.androidInfo;
-        deviceId = androidInfo.androidId;
+        deviceId = androidInfo.id;
       } else if (Platform.isIOS) {
         IosDeviceInfo iosInfo = await deviceInfoPlugin.iosInfo;
         deviceId = iosInfo.identifierForVendor;
